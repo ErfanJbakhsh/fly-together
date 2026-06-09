52a4fc759b23abcdd324505ec715a40e33b9a1fb
sisyx <sajadgone@proton.me>
2026-06-09 12:26:11 +0330

    fix: build errors

--- app/register/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { getServerSession } from "next-auth";
-import { options } from "../api/auth/[...nextauth]/options";
-import { redirect } from "next/navigation";
-
-export default async function RegisterLayout(props: any) {
-  const session = await getServerSession(options);
-  if (session) redirect("/");
-  return <>{props.children}</>;
-}
